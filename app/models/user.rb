28970eb8fab3736b7c63f9d54bb4ef8d75bba395
Chris Powell <chris.ian.powell@gmail.com>
2018-08-20 17:47:28 -0600

    adds working oauth through omniauth gem

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  def self.create_with_omni_auth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["nickname"]
+      user.token = auth["credentials"]["token"]
+      user.pic = auth["extra"]['raw_info']['avatar_url']
+    end
+  end
+end
